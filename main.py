fc966242099825e619d4ece00ecd316649baf9a6
Yashiro <customfailure00004@gmail.com>
2024-05-01 13:50:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,11 +45,11 @@ async def start_func(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 pass
             else:
                 refer_db.insert_one({"user_id":update.message.from_user.id, "refered_by":int(update.message.text.split(" ")[-1])})
-                await context.bot.send_message(chat_id=int(update.message.text.split(" ")[-1]), text=f"User_id: {update.message.from_user.id}\n\nUsed your refer link.")
-                text= f"User_id: {str(update.message.from_user.id)}\n\nUsed refer link of {str(update.message.text.split(" ")[-1])}\n\n`/info {str(update.message.from_user.id)}`\n`/info {str(update.message.text.split(" ")[-1])}`"
+                await context.bot.send_message(chat_id=int(update.message.text.split(" ")[-1]), text=f"User id: {update.message.from_user.id}\n\nUsed your refer link.")
+                text= f"User id: {str(update.message.from_user.id)}\n\nUsed refer link of {str(update.message.text.split(" ")[-1])}\n\n`/info {str(update.message.from_user.id)}`\n`/info {str(update.message.text.split(" ")[-1])}`"
                 print(text)
                 await context.bot.send_message(chat_id=-1002003442244, text=text, parse_mode=ParseMode.MARKDOWN)
-            await context.bot.send_message(chat_id=-1002003442244, text=f"User_id: {str(update.message.from_user.id)}\n\n`/info {str(update.message.from_user.id)}`", parse_mode=ParseMode.MARKDOWN)
+            await context.bot.send_message(chat_id=-1002003442244, text=f"User id: {str(update.message.from_user.id)}\n\n`/info {str(update.message.from_user.id)}`", parse_mode=ParseMode.MARKDOWN)
         else:
             await update.message.reply_text("You have already pre-registered.")
     else:
