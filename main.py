2fc398571c192ded3d7edfa1dc14ce7d138447e4
Yashiro <customfailure00004@gmail.com>
2024-05-01 13:18:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def start_func(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(is_member.status)
     if is_member.status == "LEFT" or is_member.status == "left":
         print("Left")
-    if is_member and is_member_2 and is_member.status != "left" or is_member.status_2 != "left:
+    if is_member and is_member_2 and is_member.status != "left" and is_member.status_2 != "left" and is_member.status != "Left" and is_member.status_2 != "Left":
         hmm = infodb.find_one({"user_id":update.message.from_user.id})
         if not hmm:
             infodb.insert_one({"user_id":update.message.from_user.id})
