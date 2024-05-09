1e77d3e705d756530ca9f97c7144c2fe082c8993
Yashiro <customfailure00004@gmail.com>
2024-05-09 07:54:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,9 @@ async def start_func(update: Update, context: ContextTypes.DEFAULT_TYPE):
         else:
             await update.message.reply_text("You have already pre-registered.")
     else:
-        keyboard = [[InlineKeyboardButton("Channel", url="https://t.me/Yurasania"), InlineKeyboardButton("Group", url="https://t.me/+FEQsQlSYNXNkNTc1")]]
+        link= await context.bot.create_chat_invite_link(chat_id=-1001763955719, member_limit=1,
+                                                      name=update.update.from_user.id)
+        keyboard = [[InlineKeyboardButton("Channel", url="https://t.me/Yurasania"), InlineKeyboardButton("Group", url=link.invite_link)]]
         await update.message.reply_text("Please join the following channel and use the command again to start.", reply_markup=InlineKeyboardMarkup(keyboard))
 
 async def preregcount(update: Update, context: ContextTypes.DEFAULT_TYPE):
